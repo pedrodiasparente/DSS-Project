bcc942f23ae2e2786b6ec2d54c693c2caaa78961
pedrodiasparente <pdparente@gmail.com>
2019-12-14 20:54:32 +0000

    finally

--- DSS_Projecto/src/JavaFX.java
+++ DSS_Projecto/src/JavaFX.java
@@ -1,15 +1,26 @@
 import java.io.File;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
+import javafx.scene.media.MediaErrorEvent;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class JavaFX extends Application{
+public class JavaFX extends Application
+{
+    // Create the Area for Logging
+    private TextArea messageArea = new TextArea();
     private String Dir = System.getProperty("user.dir");
     public static void main(String[] args) throws Exception{
         launch(args);
@@ -19,31 +30,116 @@ public class JavaFX extends Application{
     public void start(Stage stage) throws Exception {
 
         //goes to user Directory
-        File f = new File(Dir, "PotionSeller.mp4");
+        File f = new File(Dir, "heydarren.mp4");
 
 
         //Converts media to string URL
         Media media = new Media(f.toURI().toURL().toString());
-        javafx.scene.media.MediaPlayer player = new javafx.scene.media.MediaPlayer(media);
-        MediaView viewer = new MediaView(player);
 
-        //change width and height to fit video
-        DoubleProperty width = viewer.fitWidthProperty();
-        DoubleProperty height = viewer.fitHeightProperty();
-        width.bind(Bindings.selectDouble(viewer.sceneProperty(), "width"));
-        height.bind(Bindings.selectDouble(viewer.sceneProperty(), "height"));
-        viewer.setPreserveRatio(true);
+        // Create a Media Player
+        final MediaPlayer player = new MediaPlayer(media);
+        // Automatically begin the playback
+        player.setAutoPlay(true);
 
+        // Create a 400X300 MediaView
+        final MediaView mediaView = new MediaView(player);
+        mediaView.setFitWidth(400);
+        mediaView.setFitHeight(300);
+        mediaView.setSmooth(true);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(viewer);
+        // Create the DropShadow effect
+        DropShadow dropshadow = new DropShadow();
+        dropshadow.setOffsetY(5.0);
+        dropshadow.setOffsetX(5.0);
+        dropshadow.setColor(Color.WHITE);
 
-        //set the Scene
-        Scene scenes = new Scene(root, 1200, 800, Color.BLACK);
-        stage.setScene(scenes);
-        stage.setTitle("Media Content");
-        stage.setFullScreen(false);
+        mediaView.setEffect(dropshadow);
+
+        // Create the Buttons
+        Button playButton = new Button("Play");
+        Button stopButton = new Button("Stop");
+
+        // Create the Event Handlers for the Button
+        playButton.setOnAction(new EventHandler <ActionEvent>()
+        {
+            public void handle(ActionEvent event)
+            {
+                if (player.getStatus() == Status.PLAYING)
+                {
+                    player.stop();
+                    player.play();
+                }
+                else
+                {
+                    player.play();
+                }
+            }
+        });
+
+        stopButton.setOnAction(new EventHandler <ActionEvent>()
+        {
+            public void handle(ActionEvent event)
+            {
+                player.stop();
+            }
+        });
+
+        // Create Handlers for handling Errors
+        player.setOnError(new Runnable()
+        {
+            public void run()
+            {
+                // Handle asynchronous error in Player object.
+                printMessage(player.getError());
+            }
+        });
+
+        media.setOnError(new Runnable()
+        {
+            public void run()
+            {
+                // Handle asynchronous error in Media object.
+                printMessage(media.getError());
+            }
+        });
+
+        mediaView.setOnError(new EventHandler <MediaErrorEvent>()
+        {
+            public void handle(MediaErrorEvent event)
+            {
+                // Handle asynchronous error in MediaView.
+                printMessage(event.getMediaError());
+            }
+        });
+
+        // Create the HBox
+        HBox controlBox = new HBox(5, playButton, stopButton);
+
+        // Create the VBox
+        VBox root = new VBox(5,mediaView,controlBox,messageArea);
+
+        // Set the Style-properties of the HBox
+        root.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Add the scene to the Stage
+        stage.setScene(scene);
+        // Set the title of the Stage
+        stage.setTitle("Handling Media Errors");
+        // Display the Stage
         stage.show();
-        player.play();
+    }
+
+    private void printMessage(MediaException error)
+    {
+        MediaException.Type errorType = error.getType();
+        String errorMessage = error.getMessage();
+        messageArea.appendText("\n" + "Type:" + errorType + ", error mesage:" + errorMessage);
     }
 }

05ffb28224c34c5ef096a361a9e3f0c3ec5d2d3e
pedrodiasparente <pdparente@gmail.com>
2019-12-15 00:41:46 +0000

    media player feito

--- DSS_Projecto/src/JavaFX.java
+++ DSS_Projecto/src/JavaFX.java
@@ -58,158 +58,162 @@ public class JavaFX extends Application
         mediaView.setPreserveRatio(true);
         mediaView.setSmooth(true);
 
-        // Create the DropShadow effect, not sure if necessary
-        DropShadow dropshadow = new DropShadow();
-        dropshadow.setOffsetY(5.0);
-        dropshadow.setOffsetX(5.0);
-        dropshadow.setColor(Color.WHITE);
-        mediaView.setEffect(dropshadow);
+        // Media Ready
+        player.setOnReady(() -> {
+            System.out.println("media ready");
+            // Create Time Slider
+            Slider timeSlider = new Slider(0.0,player.getTotalDuration().toSeconds(),0.0);
 
-        // Create Volume Slider
-        final Slider volumeSlider = new Slider(0.0, 1.0, 0.5);
-        volumeSlider.setMajorTickUnit(0.01);
+            // Create Volume Slider
+            final Slider volumeSlider = new Slider(0.0, 1.0, 0.5);
+            volumeSlider.setMajorTickUnit(0.01);
 
-        // Create Time Slider
-        final Slider timeSlider = new Slider(0,6,0.0);
+            // Create the DropShadow effect, not sure if necessary
+            DropShadow dropshadow = new DropShadow();
+            dropshadow.setOffsetY(5.0);
+            dropshadow.setOffsetX(5.0);
+            dropshadow.setColor(Color.WHITE);
+            mediaView.setEffect(dropshadow);
 
-        // Bind Properties
-        player.volumeProperty().bind(volumeSlider.valueProperty());
+            // Bind Properties
+            player.volumeProperty().bind(volumeSlider.valueProperty());
 
-        // Listen to the slider. When it changes, seek with the player.
-        // MediaPlayer.seek does nothing when the player is stopped, so the play/pause button's handler
-        // always sets the start time to the slider's current value, and then resets it to 0 after starting the player.
-        InvalidationListener sliderChangeListener = o-> {
-            Duration seekTo = Duration.seconds(timeSlider.getValue());
-            player.seek(seekTo);
-        };
-        timeSlider.valueProperty().addListener(sliderChangeListener);
-
-        // Link the player's time to the slider
-        player.currentTimeProperty().addListener(l-> {
-            // Temporarily remove the listener on the slider, so it doesn't respond to the change in playback time
-            // I thought timeSlider.isValueChanging() would be useful for this, but it seems to get stuck at true
-            // if the user slides the slider instead of just clicking a position on it.
-            timeSlider.valueProperty().removeListener(sliderChangeListener);
-
-            // Keep timeText's text up to date with the slider position.
-            Duration currentTime = player.getCurrentTime();
-            int value = (int) currentTime.toSeconds();
-            timeSlider.setValue(value);
-
-            // Re-add the slider listener
+            // Listen to the slider. When it changes, seek with the player.
+            // MediaPlayer.seek does nothing when the player is stopped, so the play/pause button's handler
+            // always sets the start time to the slider's current value, and then resets it to 0 after starting the player.
+            InvalidationListener sliderChangeListener = o-> {
+                Duration seekTo = Duration.seconds(timeSlider.getValue());
+                player.seek(seekTo);
+            };
             timeSlider.valueProperty().addListener(sliderChangeListener);
-        });
 
-        // Create the GridPane Volume
-        GridPane sliderPaneVol = new GridPane();
-        sliderPaneVol.setStyle("-fx-background-color:WHITE");
+            // Link the player's time to the slider
+            player.currentTimeProperty().addListener(l-> {
+                // Temporarily remove the listener on the slider, so it doesn't respond to the change in playback time
+                // I thought timeSlider.isValueChanging() would be useful for this, but it seems to get stuck at true
+                // if the user slides the slider instead of just clicking a position on it.
+                timeSlider.valueProperty().removeListener(sliderChangeListener);
 
-        // Create the GridPane Time
-        GridPane sliderPaneTime = new GridPane();
-        sliderPaneTime.setStyle("-fx-background-color:WHITE");
+                // Keep timeText's text up to date with the slider position.
+                Duration currentTime = player.getCurrentTime();
+                int value = (int) currentTime.toSeconds();
+                timeSlider.setValue(value);
 
-        // Add the details to the GridPane
-        Label labelVolume  = new Label("Volume");
-        sliderPaneVol.addRow(0, labelVolume, volumeSlider);
-        Label labelTime  = new Label("Time");
-        sliderPaneTime.addRow(0, labelTime, timeSlider);
+                // Re-add the slider listener
+                timeSlider.valueProperty().addListener(sliderChangeListener);
+            });
 
-        // Create the Buttons
-        Button playButton = new Button("Play");
-        Button pauseButton = new Button("Pause");
-        Button stopButton = new Button("Stop");
-        Button closeButton = new Button("Close");
+            // Create the GridPane Volume
+            GridPane sliderPaneVol = new GridPane();
+            sliderPaneVol.setStyle("-fx-background-color:WHITE");
 
-        // Create the Event Handlers for the Button
-        playButton.setOnAction(new EventHandler <ActionEvent>()
-        {
-            public void handle(ActionEvent event)
+            // Create the GridPane Time
+            GridPane sliderPaneTime = new GridPane();
+            sliderPaneTime.setStyle("-fx-background-color:WHITE");
+
+            // Add the details to the GridPane
+            Label labelVolume  = new Label("Volume");
+            sliderPaneVol.addRow(0, labelVolume, volumeSlider);
+            Label labelTime  = new Label("Time");
+            sliderPaneTime.addRow(0, labelTime, timeSlider);
+
+            // Create the Buttons
+            Button playButton = new Button("Play");
+            Button pauseButton = new Button("Pause");
+            Button replayButton = new Button("Replay");
+            Button closeButton = new Button("Close");
+
+            // Create the Event Handlers for the Button
+            playButton.setOnAction(new EventHandler <ActionEvent>()
             {
-                if (player.getStatus() == Status.PLAYING)
+                public void handle(ActionEvent event)
                 {
-                    //
+                    if (player.getStatus() == Status.PLAYING)
+                    {
+                        //
+                    }
+                    else
+                    {
+                        player.play();
+                    }
                 }
-                else
+            });
+
+            replayButton.setOnAction(new EventHandler <ActionEvent>()
+            {
+                public void handle(ActionEvent event)
                 {
-                    player.play();
+                    player.seek(player.getStartTime());
                 }
-            }
+            });
+
+            pauseButton.setOnAction(new EventHandler <ActionEvent>()
+            {
+                public void handle(ActionEvent event)
+                {
+                    player.pause();
+                }
+            });
+
+            closeButton.setOnAction(new EventHandler <ActionEvent>()
+            {
+                public void handle(ActionEvent event)
+                {
+                    stage.close();
+                }
+            });
+
+            // Create Handlers for handling Errors
+            player.setOnError(new Runnable()
+            {
+                public void run()
+                {
+                    // Handle asynchronous error in Player object.
+                    printMessage(player.getError());
+                }
+            });
+
+            media.setOnError(new Runnable()
+            {
+                public void run()
+                {
+                    // Handle asynchronous error in Media object.
+                    printMessage(media.getError());
+                }
+            });
+
+            mediaView.setOnError(new EventHandler <MediaErrorEvent>()
+            {
+                public void handle(MediaErrorEvent event)
+                {
+                    // Handle asynchronous error in MediaView.
+                    printMessage(event.getMediaError());
+                }
+            });
+
+            // Create the HBox
+            HBox controlBox = new HBox(5, sliderPaneVol, playButton, pauseButton, replayButton,closeButton, sliderPaneTime);
+            controlBox.setAlignment(Pos.BASELINE_CENTER);
+
+            // Create the VBox
+            VBox root = new VBox(-40,mediaView,controlBox/*,messageArea*/);
+            root.setAlignment(Pos.BASELINE_CENTER);
+
+            // Create the Scene
+            Scene scene = new Scene(root, 1200, 675);
+            // Add the scene to the Stage
+            stage.setScene(scene);
+            // Set the title of the Stage
+            stage.setTitle("Media Content");
+            // Display the Stage
+            stage.show();
         });
 
-        stopButton.setOnAction(new EventHandler <ActionEvent>()
-        {
-            public void handle(ActionEvent event)
-            {
-                player.stop();
-            }
-        });
-
-        pauseButton.setOnAction(new EventHandler <ActionEvent>()
-        {
-            public void handle(ActionEvent event)
-            {
-                player.pause();
-            }
-        });
-
-        closeButton.setOnAction(new EventHandler <ActionEvent>()
-        {
-            public void handle(ActionEvent event)
-            {
-                stage.close();
-            }
-        });
-
-        // Create Handlers for handling Errors
-        player.setOnError(new Runnable()
-        {
-            public void run()
-            {
-                // Handle asynchronous error in Player object.
-                printMessage(player.getError());
-            }
-        });
-
-        media.setOnError(new Runnable()
-        {
-            public void run()
-            {
-                // Handle asynchronous error in Media object.
-                printMessage(media.getError());
-            }
-        });
-
-        mediaView.setOnError(new EventHandler <MediaErrorEvent>()
-        {
-            public void handle(MediaErrorEvent event)
-            {
-                // Handle asynchronous error in MediaView.
-                printMessage(event.getMediaError());
-            }
-        });
 
         // If we want to do stuff no fim
         player.setOnEndOfMedia(() -> {
             System.out.println("end of media");
-            System.out.println(player.getStatus());
         });
-
-        // Create the HBox
-        HBox controlBox = new HBox(5, sliderPaneVol, playButton, pauseButton, stopButton,closeButton, sliderPaneTime);
-        controlBox.setAlignment(Pos.BASELINE_CENTER);
-
-        // Create the VBox
-        VBox root = new VBox(-40,mediaView,controlBox/*,messageArea*/);
-        root.setAlignment(Pos.BASELINE_CENTER);
-
-        // Create the Scene
-        Scene scene = new Scene(root, 1200, 675);
-        // Add the scene to the Stage
-        stage.setScene(scene);
-        // Set the title of the Stage
-        stage.setTitle("Media Content");
-        // Display the Stage
-        stage.show();
     }
 
     private void printMessage(MediaException error)

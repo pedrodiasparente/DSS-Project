e6501f38658f9ecc829c786452795d563a2430dc
pedrodiasparente <pdparente@gmail.com>
2019-12-15 22:16:11 +0000

    playsound e main deleted?

--- DSS_Projecto/src/MediaCenter/Main.java
+++
@@ -1,8 +0,0 @@
-package MediaCenter;
-
-public class Main {
-    public static void main(String[] args) {
-        PlaySound.play("In_a_Perfect_World.mp3");
-        PlaySound.play("PotionSeller.mp4");
-    }
-}

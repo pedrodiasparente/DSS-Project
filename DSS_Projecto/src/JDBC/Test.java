468331bb356067265fcf85d76af211d7c598002d
pedrodiasparente <pdparente@gmail.com>
2019-12-16 00:44:10 +0000

    do work now nuno

---
+++ DSS_Projecto/src/JDBC/Test.java
@@ -0,0 +1,27 @@
+package JDBC;
+
+import MediaCenter.Utilizador;
+
+import java.sql.*;
+
+public class Test {
+
+    public Test() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            //Classe nao disponivel
+        }
+
+        public Utilizador get(Object key){
+            try (Connection con = DriverManager.getConnection("jdbc:mysql ://localhost/ DBmodel?user = un&password = pwd ") {
+
+            }
+            catch (SQLException e) {
+                // Erro ao establecer ligaçao
+            }
+
+        }
+    }
+}
